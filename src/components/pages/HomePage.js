a916718d8269bdb3675f9430aeb18fbb8db5228f
Sai Manasa Yadlapalli <manasa.yadlapalli5@gmail.com>
2022-11-13 20:36:43 -0800

    Added graph to homepage

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,8 +1,40 @@
 import React from 'react'
 import Table from 'react-bootstrap/Table';
+import { Chart } from "react-google-charts";
+
 import '../../styles/Buttons.css'
 
 export default function HomePage() {
+
+    const options = {
+        chart: {
+          title: "30 days ",
+          subtitle: "7 days",
+        },
+      };
+    const data = [
+        [
+          "Day",
+          "Electricity Meter 1",
+          "Electricity Meter 2",
+          "Electricity Meter 3",
+        ],
+        [1, 37.8, 10.8, 41.8],
+        [2, 30.9, 33.5, 50.4],
+        [3, 25.4, 57, 25.7],
+        [4, 11.7, 18.8, 10.5],
+        [5, 11.9, 17.6, 10.4],
+        [6, 8.8, 13.6, 7.7],
+        [7, 7.6, 12.3, 20.6],
+        [8, 12.3, 29.2, 10.6],
+        [9, 16.9, 42.9, 14.8],
+        [10, 12.8, 30.9, 11.6],
+        [11, 5.3, 7.9, 4.7],
+        [12, 6.6, 8.4, 5.2],
+        [13, 4.8, 6.3, 3.6],
+        [14, 50.2, 44.2, 3.4],
+      ];
+
     return (
         <>
         <div className="text-left">
@@ -47,7 +79,17 @@ export default function HomePage() {
                     <th></th>
                 </tr>
                 </thead>
-               </Table> 
+               </Table>  <br/>
+               <div>
+                <Chart
+                    chartType="Line"
+                    width="90%"
+                    height="300px"
+                    data={data}
+                    options={options}
+                    />
+               </div>
+
        
         </div>
         </>
